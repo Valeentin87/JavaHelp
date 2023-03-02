223024aa1f296c0eb7d8f1161c9ba6fbbba81fa0
Valeentin87 <valeentin87@list.ru>
2023-03-02 13:45:52 +0300

    Добавил файл со шпорой по синтаксису Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import static java.lang.Character.getType;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        System.out.println(a++);
+        System.out.println(a);
+
+
+    }
+}
